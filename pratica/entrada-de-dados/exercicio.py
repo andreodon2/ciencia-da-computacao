62e80ea2829e15dd53a0e9b1e6bff5eca89df214
André Gabriel Odon de Freitas <andreodon@gmail.com>
2025-01-09 17:47:38 -0300

    Envio de exercícios

---
+++ pratica/entrada-de-dados/exercicio.py
@@ -0,0 +1,30 @@
+a = int(input("Qual o valor de a? "))
+b = int(input("Qual o valor de b? "))
+aux = a
+a = b
+b = aux
+print(a)
+print(b)
+
+# a = int(input("Qual o valor de a? "))
+# b = int(input("Qual o valor de b? "))
+# a = b
+# b = a
+# print(a)
+# print(b)
+
+# a = int(input("Qual o valor de a? "))
+# b = int(input("Qual o valor de b? "))
+# aux = a
+# a = b
+# b = aux
+# print(a)
+# print(b)
+
+
+# a = int(input("Qual o valor de a? "))
+# b = int(input("Qual o valor de b? "))
+# a = b
+# b = a
+# print(a)
+# print(b)
