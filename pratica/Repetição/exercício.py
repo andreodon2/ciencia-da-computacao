714127b4b13a69d16a6dfea22c5e7122f8afbbfd
André Gabriel Odon de Freitas <andreodon@gmail.com>
2025-01-14 17:05:17 -0300

    Exercícios de repetição

---
+++ pratica/Repetição/exercício.py
@@ -0,0 +1,27 @@
+i = 6
+while (i > 0):          
+    i = i - 3    
+    print (i) 
+
+# count = 0
+# while (count < 10):
+#      # Ponto A
+#      print ("Olá...", count)
+#      count =  count + 1
+#      # Ponto B
+# # Ponto C
+
+# i = 2
+# while i % 3 != 0:
+#     print(i)
+#     i += 2
+
+# i = 0
+# while (i < 5):
+#   print (i) 
+#   i = i + 1
+
+# x = 1
+# while (x < 4):
+#     x = x + 1
+# print ("x vale", x)
